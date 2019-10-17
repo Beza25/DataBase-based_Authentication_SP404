c081bfaaf0c6c900203fdc294857ac5f84bdb553
Beza25 <sirak.bezawit@gmail.com>
2019-10-17 15:01:46 -0400

    it works!

--- src/main/java/com/example/demo/User.java
+++ src/main/java/com/example/demo/User.java
@@ -44,7 +44,7 @@ public class User {
     public User(String email, String password, String firstName, String lastName,
                 boolean enabled, String username){
         this.email = email;
-        this.password = password;
+         setPassword(password);
         this.firstName = firstName;
         this.lastName = lastName;
         this.enabled = enabled;
